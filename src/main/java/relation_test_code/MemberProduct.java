f971397a5cd12173e7b158f20fa9af06670d1c5e
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-08 22:53:39 +0900

    다대다 관계2

---
+++ src/main/java/relation_test_code/MemberProduct.java
@@ -0,0 +1,18 @@
+package relation_test_code;
+
+import javax.persistence.*;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member memeber;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+}
