2ff0206f40109ed16995e4e39546af9038489736
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-10 21:49:58 +0900

    프록시 지연로딩

--- src/main/java/relation_test_code/Member.java
+++ src/main/java/relation_test_code/Member.java
@@ -13,8 +13,8 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     @OneToOne
