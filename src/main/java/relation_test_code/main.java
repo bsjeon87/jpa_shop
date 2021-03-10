2af79b69031677d697a0e694c68cafdd250f3d97
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-10 22:08:37 +0900

    영속성 전이 cascade

--- src/main/java/relation_test_code/main.java
+++ src/main/java/relation_test_code/main.java
@@ -15,14 +15,19 @@ public class main {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
 
-            Locker locker = new Locker();
-            locker.setName("locker");
-            locker.setMember(member);
-            em.persist(locker);
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+          //  em.persist(child1); //영속성 전이 / cascade를 선억하면 연관된 child에 대한 영속성 등록이 필요없음.(연관관계와 상관없음.)
+          //  em.persist(child2); //주의: cascade를 ALL로 선언할때는 다른 객체와 연관관계가 없고,
+                                 // 확실한 라이프 사이클이 동일할때만 사용해야함 - 완전종속/단일소유( 예: 게시물에 포함된 첨부파일 객체..)
+                                  //그렇지 않고, 다른 객체와도 연관관계가 있는 경우, 문제가 발생될수있음.
 
             tx.commit();
         } catch (Exception e) {
