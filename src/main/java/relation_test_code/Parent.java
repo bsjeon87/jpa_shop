ad33c7e1d8b6af0cdf8c598b3f4afb61bfbcb653
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-12 05:56:51 +0900

    cascade / orphanRemoval

--- src/main/java/relation_test_code/Parent.java
+++ src/main/java/relation_test_code/Parent.java
@@ -12,8 +12,11 @@ public class Parent {
     private Long id;
 
     private String name;
-
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+                                                                                     //cascade => list에 추가되는 노드는 자동으로 영속성 관리됨(parent에서 영속성객체에 속할때).
+                                                                                     //의존관계주인 개념과는 별개의 개념임.
+                                                                                     //orphanReomoval이 켜져있지 않으면 parent가 영속성 객체에서 빠지기 전까지는 child의 노드가
+                                                                                     //삭제되더라도 child를 테이블에서 삭제하지않음.(실제 child객체에서 parent를 빼기전까지..->의존성 주인)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true) //orphanRemoval => list에서 하나의 노드를 뺄때 해당 노드를 테이블에서 삭제.
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
@@ -21,6 +24,9 @@ public class Parent {
         child.setParent(this);
     }
 
+    public List<Child> getList() {
+        return childList;
+    }
     public String getName() {
         return name;
     }
--- src/main/java/relation_test_code/main.java
+++ src/main/java/relation_test_code/main.java
@@ -24,6 +24,10 @@ public class main {
             parent.addChild(child2);
 
             em.persist(parent);
+            Child child3= new Child();
+            parent.addChild(child3);
+
+            parent.getList().remove(child1);
           //  em.persist(child1); //영속성 전이 / cascade를 선억하면 연관된 child에 대한 영속성 등록이 필요없음.(연관관계와 상관없음.)
           //  em.persist(child2); //주의: cascade를 ALL로 선언할때는 다른 객체와 연관관계가 없고,
                                  // 확실한 라이프 사이클이 동일할때만 사용해야함 - 완전종속/단일소유( 예: 게시물에 포함된 첨부파일 객체..)
