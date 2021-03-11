ce6ce31005aa342c0099e639fa5305a359c9ceac
Bongsu Jeon <bs.jeon87@gmail.com>
2021-03-12 07:26:40 +0900

    값타입 컬렉션

--- src/main/java/relation_test_code/Member.java
+++ src/main/java/relation_test_code/Member.java
@@ -4,7 +4,9 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -31,6 +33,19 @@ public class Member {
     //private String zipcode;
     @Embedded
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME") //String하나이므로 column지정이 바로 가능한 케이스.(Address의 경우 Address내에 컬럼지정해놓음.)
+    private Set<String> favoriteFods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressesHistory = new ArrayList<>();
 
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
